ac35cbb03c5636a134ad8b89d0de437c96dc9692
Jvazqu61 <jvazqu61@uic.edu>
2022-01-11 19:01:40 -0600

    path edit

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,13 +4,14 @@ import { NextResponse } from "next/server";
 export async function middleware(req){
     const token =  await getToken({ req, secret: process.env.JWT_SECRET});
     const { pathname } = req.nextUrl
-    if ((pathname.includes('/api/auth') || token) && pathname !== '/login'){
+    if ((pathname.includes('/api/auth') || token)){
+        
         return NextResponse.next();
     }
 
-    if (token && pathname === '/login'){
-        return NextResponse.redirect("/")
-    }
+    // if (token && pathname === '/login'){
+    //     return NextResponse.redirect("/")
+    // }
 
     if (!token && pathname !== '/login'){
         return NextResponse.redirect("/login")
